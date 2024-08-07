8d35aed07e2df6cfed8fd9e179cbd90f280526f6
sthfsuh154 <sthfsuh154@gmail.com>
2024-08-07 11:15:05 +0530

    play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -42,7 +42,7 @@ SPAM_THRESHOLD = 2
 SPAM_WINDOW_SECONDS = 5
 
 @app.on_message(
-   filters.command(["play", "vplay", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce"], prefixes=["/", "!", "%", "", ".", "@", "#"])
+   filters.command(["ply", "vply", "cply", "cvply", "plyforce", "vplyforce", "cplyforce", "cvplyforce"], prefixes=["/", "!", "%", "", ".", "@", "#"])
             
     & filters.group
     & ~BANNED_USERS
